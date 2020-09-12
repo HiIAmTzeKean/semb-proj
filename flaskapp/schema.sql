95166c699f006390ef342db2b9f8991ffde41a67
HiIAmTzeKean <44837096+ngtzekean600@users.noreply.github.com>
2020-09-12 15:34:18 +0800

    ver 3

--- flaskapp/schema.sql
+++ flaskapp/schema.sql
@@ -1,8 +1,22 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS personnel;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+CREATE TABLE "user" (
+	"id"	INTEGER,
+	"username"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "personnel" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"vocation"	TEXT DEFAULT 'Auto Tech',
+	"fmw"	TEXT DEFAULT 'Sembawang',
+	"rank"	TEXT,
+	"am_status"	TEXT,
+	"am_remarks"	TEXT,
+	"pm_status"	TEXT,
+	"pm_remarks"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
