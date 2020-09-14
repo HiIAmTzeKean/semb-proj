29da7ea46d20f7ddc85694ba631b575d62e35d45
HiIAmTzeKean <44837096+ngtzekean600@users.noreply.github.com>
2020-09-14 13:19:14 +0800

    ver4

--- flaskapp/methods.py
+++ flaskapp/methods.py
@@ -3,3 +3,20 @@ def converter_paradestateform(rows):
     for row in rows:
         names.append((row["name"]))
     return names
+
+import os
+from csv import writer, DictWriter
+from datetime import date
+def generate_PS():
+    # create func to write per day
+    date_today = date.today().strftime("%d%m%Y")
+    # create file and open in write mode
+    csv_name = os.path.join('excel_sheets','{}.csv'.format(date_today))
+    with open(csv_name, 'w', newline='') as csvfile:
+        # write date first
+        csvwriter = writer(csvfile)
+        csvwriter.writerow(['Date',date_today])
+        # write data from data base
+        fieldnames = ['fmw','Name','Rank','Am status','Am Remarks','Pm status','Pm Remarks']
+        csvwriter = DictWriter(csvfile, fieldnames=fieldnames)
+        csvwriter.writeheader()
