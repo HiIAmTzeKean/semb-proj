df58978d6a71b449a705a52af9b35325fc4afb0a
Chan Kah Leong <chankahleong@outlook.com>
2020-09-26 23:06:19 +0800

    Bug fix in misc.py

--- flaskapp/misc.py
+++ flaskapp/misc.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, g, redirect, render_template, url_for
+    Blueprint, g, redirect, render_template, url_for, request
 )
 from flaskapp.db import get_db
 from .forms import paradestateform
@@ -7,18 +7,20 @@ import git
 
 bp = Blueprint('misc', __name__)
 
+
 # successful upload page
 @bp.route('/success', methods=('GET', 'POST'))
 def success():
     return render_template('misc/success.html')
 
+
 # generate route for webhook
 @bp.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('https://github.com/HiIAmTzeKean/semb-proj.git')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/HiIAmTzeKean/semb-proj.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
